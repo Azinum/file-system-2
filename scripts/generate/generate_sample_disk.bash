4f6e76950ee9b5f1cc8f0dfd92f0682a9095d474
azinum <lukaska61@gmail.com>
2020-01-10 17:05:05 +0100

    Update generate_sample_disk.bash to match the new commandline argument options

--- scripts/generate/generate_sample_disk.bash
+++ scripts/generate/generate_sample_disk.bash
@@ -6,17 +6,17 @@
 function start() {
 	./file_system
 	info_file='data.txt'
-	./file_system c ${info_file}
+	./file_system -c ${info_file}
 	shopt -s dotglob
 	shopt -s nullglob
 	files=(scripts/generate/sample/*)
 	for file in ${files[@]}; do
-		./file_system a ${info_file} ${file}"
+		./file_system -a ${info_file} ${file}"
 "	# To add a newline
 		if [ -d $file ]; then
-			./file_system d "$(basename "$file")"
+			./file_system -d "$(basename "$file")"
 		elif [ -f $file ]; then
-			./file_system w "$(basename "$file")" "$(cat ${file})"
+			./file_system -w "$(basename "$file")" "$(cat ${file})"
 		fi
 	done
 }
